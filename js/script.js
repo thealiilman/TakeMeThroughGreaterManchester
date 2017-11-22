d427dbaccf9865f241200019ed47ea706945c7e7
Ali Ilman <ali@ali-ilman.com>
2017-11-23 07:57:10 +0800

    Added parallax transition

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
-var parallax = document.getElementById("parallax-container");
+const parallax = document.getElementById("parallax"),
+      images = ["../images/manc1.jpg", "../images/manc2.jpg", "../images/manc3.jpg"];
+let slide = 1;
+parallax.style.backgroundImage = "url("+images[slide-1]+")";
 
-function parallaxSlideshow() {
-  var images = ["../images/manc1.jpg", "../images/manc2.jpg", "../images/manc3.jpg"];
-  parallax.style.backgroundImage = url();
+setInterval(imageSlideshow, 3000);
+
+function imageSlideshow() {
+  switch(slide) {
+    case 0:
+      parallax.style.backgroundImage = "url("+images[slide]+")";
+      slide++;
+    break;
+    case 1:
+      parallax.style.backgroundImage = "url("+images[slide]+")";
+      slide++;
+    break;
+    case 2:
+      parallax.style.backgroundImage = "url("+images[slide]+")";
+      slide = 0;
+    break;
+    default:
+      null;
+    break;
+  }
 }
-
-parallaxSlideshow();
